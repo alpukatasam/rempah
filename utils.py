2f647278b3263c658bcf754bba28dbf67a732a34
alpukatasam <132274063+alpukatasam@users.noreply.github.com>
2025-06-16 17:24:17 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-def download_model_from_gdrive(file_id, output_path='model.keras'):
+def download_model_from_gdrive(file_id, output_path):
     url = f'https://drive.google.com/uc?id={file_id}'
     response = requests.get(url, stream=True)
     with open(output_path, 'wb') as f:
